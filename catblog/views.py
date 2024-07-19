069c682235c02ec8ecdc3aab09eab343e704539f
nugiemiko <nugroho.wijatmiko@gmail.com>
2024-07-19 14:33:59 +0700

    fix query

--- catblog/views.py
+++ catblog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.contrib.auth.models import User
 
 def post_list(request):
+    me = User.objects.get(username='admin')
     #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    posts = Post.objects.filter(author=User.objects.get(username='admin')).order_by('published_date')
+    posts = Post.objects.filter(author=me).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
